8dcb7b9479a253c2e93eed10fdec39f2835102f7
Avinag <udayagiriavinag@gmail.com>
2025-04-30 07:33:29 +0530

    Implementation of Lo-Fi of introduction/home/greeting section

--- src/app/home.ts
+++ src/app/home.ts
@@ -1,4 +1,5 @@
 import { loadTemplate } from '../utils/loadTemplate';
+import './home.scss';
 export class HomeComponent {
   constructor() {
     loadTemplate('/src/app/home.html').then((html) => {
@@ -8,4 +9,38 @@ export class HomeComponent {
       }
     });
   }
+  technicalSkills = [
+    {
+      name: 'Angular',
+      image: '/assets/angular.svg',
+    },
+    {
+      name: 'Ionic',
+      image: '/assets/ionic.svg',
+    },
+    {
+      name: 'TypeScript',
+      image: '/assets/typescript.svg',
+    },
+    {
+      name: 'React Native/Expo',
+      image: '/assets/react-native.svg',
+    },
+    {
+      name: 'HTML',
+      image: '/assets/html.svg',
+    },
+    {
+      name: 'CSS',
+      image: '/assets/css.svg',
+    },
+    {
+      name: 'Sass',
+      image: '/assets/sass.svg',
+    },
+    {
+      name: 'Rust',
+      image: '/assets/rust.svg',
+    },
+  ];
 }
